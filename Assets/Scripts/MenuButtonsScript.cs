26b88bc90a6a08a3aef1951679bc980fbd3d6bc3
gshantop <gchechotkin@gmail.com>
2026-09-24 12:05:35 +0300

    Uztaisiju sakumu ekranu

---
+++ Assets/Scripts/MenuButtonsScript.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtonsScript : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+    }
+}
